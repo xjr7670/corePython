fb20f3b3414b35f9947d19899e841f84b6d520f1
Ubuntu <xjr30226@126.com>
2016-01-06 21:26:19 +0800

    add 2-4

---
+++ 2-4a.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+
+s = raw_input("Please input a string: ")
+print s
---
+++ 2-4b.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+
+n = raw_input("Please input a number: ")
+m = int(n)
+print m
